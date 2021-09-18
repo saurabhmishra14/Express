fe87138f193fcc888a3f20a3afd2dbef1c23ed57
Saurabh Mishra <saurabh@newput.com>
2021-09-18 08:40:56 +0530

    modified

--- BodyParser/bodyparser.js
+++ BodyParser/bodyparser.js
@@ -4,16 +4,18 @@ let app = express();
 // For POST-Support
 let bodyParser = require('body-parser');
 
-app.use(bodyParser.json());//   For JSON        
-app.use(bodyParser.urlencoded({ extended: false}));//Parse the request body into JS Object ( if content-type is application/JSON)
+app.use(bodyParser.json()); //   For JSON        
+app.use(bodyParser.urlencoded({
+    extended: false
+})); //Parse the request body into JS Object ( if content-type is application/JSON)
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/', (request, response) => {
     let name = request.body.name;
-    let age= request.body.age;
-    
-    console.log(' Name: ',name);
-    console.log(' Age: ',age);
-    
+    let age = request.body.age;
+
+    console.log(' Name: ', name);
+    console.log(' Age: ', age);
+
 });
 
 app.listen(3000);
